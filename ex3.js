3ca5ac9c27ad377140aac5901803eed39a797a06
wladilewis <89107641+wladilewis@users.noreply.github.com>
2021-10-25 23:19:50 -0300

    Aula 04

---
+++ ex3.js
@@ -0,0 +1,46 @@
+// Lucas Casasco, Wladi, Ruggiero e Jadson
+
+
+// 1) Em um novo arquivo chamado ex3.js, exiba na tela a soma de dois números
+
+console.log(1 + 2);
+
+//2) Repita o processo anterior, mas utilizando outro operador.
+console.log(5-2);
+
+//3) Declare e atribua 2 variáveis ​​numéricas independentes. Exiba a soma de ambas na tela.
+let x = 5;
+
+let y = 7;
+
+console.log(x+y);
+
+//4) Atribua a soma das duas variáveis ​​anteriores a uma outra variável e exiba-a na tela.
+let soma = x + y;
+
+console.log(soma);
+
+//5) Re-atribua à variável anterior "1" +1, e exiba na tela. (O que  aconteceu?)
+
+soma = "1"+1; //deu o resultado 11.
+
+console.log(soma);
+
+//6) Re-atribua "olá" + 23, e exiba na tela. (O que aconteceu?)
+soma = 'ola'+ 23;
+console.log(soma); // junto o texto e os numeros
+
+// 7) Crie mensagens concatenadas com valores diferentes
+
+let nome = 'Wladi';
+let cidade = 'Sao Paulo';
+
+console.log(nome+ ' nasceu em '+cidade);
+
+
+
+
+
+
+
+
